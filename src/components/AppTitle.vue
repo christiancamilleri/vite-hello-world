8d656f8130c8131aa3ba719169753aaf5825cdd1
christiancamilleri <camilleri.christian@outlook.com>
2023-03-23 14:47:36 +0100

    feat: exercise without bonus

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script >
+
+export default {
+    data() {
+        return {
+            titolo: "La mia prima app con Vite"
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <h1> {{ titolo }} </h1>
+    </div>
+</template>
+
+<style ></style>
